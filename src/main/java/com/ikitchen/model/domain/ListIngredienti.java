22d2aa9ee788f7d1cadc2f3530f7268c751335b3
Marco Cozzolino <marco.navigazione@gmail.com>
2024-09-08 17:20:32 +0200

    Risoluzione piccolo code smell

--- src/main/java/com/ikitchen/model/domain/ListIngredienti.java
+++ src/main/java/com/ikitchen/model/domain/ListIngredienti.java
@@ -1,16 +1,17 @@
 package com.ikitchen.model.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListIngredienti {
+public class ListIngredienti implements Serializable {
     List<Ingrediente> listaIngredienti = new ArrayList<>();
 
     public void addIngrediente(Ingrediente ingrediente) {
         this.listaIngredienti.add(ingrediente);
     }
 
-    public List<Ingrediente> getListaIngredienti(){
+    public List<Ingrediente> getListaIngredienti() {
         return listaIngredienti;
     }
 }
